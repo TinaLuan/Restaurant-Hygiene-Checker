fa55192c81377bda5e58b1c2945ebd1393c0492e
TinaLuan <tinaluan9794@gmail.com>
2018-03-07 17:22:46 +0000

    selected items can be recorded

--- app/src/main/java/tluan/restauranthygienechecker/FilterActivity.java
+++ app/src/main/java/tluan/restauranthygienechecker/FilterActivity.java
@@ -31,7 +31,7 @@ public class FilterActivity extends AppCompatActivity {
      ArrayList<Integer> businessTypesIDList = new ArrayList<>();
      ArrayList<Integer> ratingIDList = new ArrayList<>();
      ArrayList<Integer> countryIDList = new ArrayList<>();
-
+    ArrayList<Integer> authorityIDList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,11 +76,11 @@ public class FilterActivity extends AppCompatActivity {
         authorityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         authoritySpinner.setAdapter(authorityAdapter);
 
-        Spinner regionSpinner = (Spinner)findViewById(R.id.region);
-        final ArrayAdapter<String> regionAdapter = new ArrayAdapter<String>(getApplicationContext(),
+        Spinner countrySpinner = (Spinner)findViewById(R.id.country);
+        final ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(getApplicationContext(),
                 android.R.layout.simple_spinner_item, android.R.id.text1);
-        regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        regionSpinner.setAdapter(regionAdapter);
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner.setAdapter(countryAdapter);
 
 
 
@@ -97,59 +97,37 @@ public class FilterActivity extends AppCompatActivity {
                 "Ratings", "ratings", "ratingName",
                 "ratingId", ratingAdapter, ratingIDList);
         requestQueue.add(objRequest2);
-/*
-        final String businessTypesSuffix = "BusinessTypes/basic";
-        MyJsonObjectRequest objRequest1 = new MyJsonObjectRequest(businessTypesSuffix,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
 
-                        try {
-                            JSONArray array = response.getJSONArray("businessTypes");
-                            for (int i=0; i<array.length(); i++) {
-                                String type = array.getJSONObject(i).getString("BusinessTypeName");
-                                int id = array.getJSONObject(i).getInt("BusinessTypeId");
-                                businessTypeAdapter.add(type);
-                                businessTypesIDList.add(id);
-                            }
-                            businessTypeAdapter.notifyDataSetChanged();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        );
-        requestQueue.add(objRequest1);
+        MyJsonObjectRequest objRequest3 = new MyJsonObjectRequest(
+                "Countries/basic", "countries", "name",
+                "id", countryAdapter, countryIDList);
+        requestQueue.add(objRequest3);
 
-
-        final String ratingSuffix = "Ratings";
-        MyJsonObjectRequest objRequest2 = new MyJsonObjectRequest(ratingSuffix,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        try {
-                            JSONArray array = response.getJSONArray("ratings");
-                            for (int i=0; i<array.length(); i++) {
-                                String ratingName = array.getJSONObject(i).getString("ratingName");
-                                int id = array.getJSONObject(i).getInt("ratingId");
-
-                                ratingAdapter.add(ratingName);
-                                ratingIDList.add(id);
-                            }
-                            ratingAdapter.notifyDataSetChanged();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        );
-*/
+        MyJsonObjectRequest objRequest4 = new MyJsonObjectRequest(
+                "Authorities/basic", "authorities", "Name",
+                "LocalAuthorityId", authorityAdapter, authorityIDList);
+        requestQueue.add(objRequest4);
 
     }
 
 
     public void onDone() {
+
+
+//        AdapterView.OnItemSelectedListener myListener = new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+//                Log.d("text", parentView.getItemAtPosition(position).toString());
+//                Log.d("id", String.valueOf(businessTypesIDList.get(position)));
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parentView) {
+//                // your code here
+//            }
+//
+//        };
+
         Spinner businessTypeSpinner = (Spinner)findViewById(R.id.businessType);
 
         businessTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -182,7 +160,42 @@ public class FilterActivity extends AppCompatActivity {
 
         });
 
+        Spinner authoritySpinner = (Spinner)findViewById(R.id.authority);
+
+        authoritySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                Log.d("text", parentView.getItemAtPosition(position).toString());
+                Log.d("id", String.valueOf(authorityIDList.get(position)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+        Spinner countrySpinner = (Spinner)findViewById(R.id.country);
+
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                Log.d("text", parentView.getItemAtPosition(position).toString());
+                Log.d("id", String.valueOf(countryIDList.get(position)));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
     }
 
+//    private MyOnItemSelectedListener extends AdapterView.OnItemSelectedListener{
+//
+//    }
 
 }
